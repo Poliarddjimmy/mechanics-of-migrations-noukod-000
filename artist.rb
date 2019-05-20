1d749c8896a30f50870f7e6900a0a0f626fb6432
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-20 19:15:52 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,26 +1,3 @@
 class Artist < ActiveRecord::Base
-  attr_accessor :name, :age, :genre, :hometown
-  
-  attr_reader :id
-  
-  def initialize(name, age, genre, hometown, id=nil)
-    @id = id
-    @name = name
-    @age = age
-    @genre = genre
-    @hometown = hometown
-  end
-  
-  def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS artists (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        age INTEGER,
-        genre text,
-        hometown text
-        )
-    SQL
-    DB[:conn].execute(sql) 
-  end
+ 
 end
