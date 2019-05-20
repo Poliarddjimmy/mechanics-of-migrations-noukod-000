c0dbc01e0af79d9c978be321c6cec354942057a2
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-20 19:09:51 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   
   attr_reader :id
   
-  def initialize(name, grade, id=nil)
+  def initialize(name, age, genre, hometown, id=nil)
     @id = id
     @name = name
     @grade = grade
