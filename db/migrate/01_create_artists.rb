086d0e2cfb4753263079321d4f2d20a7d50b5141
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-20 19:12:28 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,5 @@
 class CreateArtists < ActiveRecord::Migration
   def up
-    create_table :artists do |a|
-      a.string :name
-      a.string :genre
-      a.integer :age
-      a.string :hometown
-    end
   end
 
   def down
